23fc000ff4e3f9c8b2391ba10804fa66008573dc
Void7 <limen191100@gmail.com>
2021-10-23 20:29:52 +0300

    Init window
    Add some consts to init window Dapper Dasher.
    Add game loop.
    Call function CloseWindow at the end of program.

---
+++ dasher.cpp
@@ -0,0 +1,16 @@
+#include "raylib.h"
+
+int main()
+{
+    // Window dimensions
+    int const width{800}, height{400};
+    InitWindow(width, height, "Dapper Dasher");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+        EndDrawing();
+    }
+}
