e1a098f56acdcd8338567401fd805933bb91cfac
Void7 <limen191100@gmail.com>
2021-10-25 23:47:48 +0300

    IsInAir and Jump velocity
    Add jump velocity.
    Add isInAir check that prevent jumping in air.

--- dasher.cpp
+++ dasher.cpp
@@ -12,19 +12,26 @@ int main()
     const int r_width{50}, r_height{50};
     const Color r_color{BLUE};
     int posX{width/2}, posY{height/2};
+    bool isInAir{false};
+    const int j_velocity{25};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
         // Gravity and Ground check
-        if (posY + r_height < height) {velocity += gravity;} 
+        if (posY + r_height < height) 
+        {
+            isInAir = true;
+            velocity += gravity;
+        } 
         else 
         {
+            isInAir = false;
             velocity = 0;
             posY = height - r_height;
         }
         // Jump 
-        if (IsKeyPressed(KEY_SPACE)) {velocity -=10;}
+        if (IsKeyPressed(KEY_SPACE) && !isInAir) {velocity -=j_velocity;}
         // Location
         posY += velocity;
 
