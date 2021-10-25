371b87917df30409882fb474bb60409be8ce07bf
Void7 <limen191100@gmail.com>
2021-10-25 23:12:34 +0300

    Rectangle, Velocity and Jump
    Add draw rectangle.
    Add velocity.
    Add check if space pressed then jump.

--- dasher.cpp
+++ dasher.cpp
@@ -5,12 +5,17 @@ int main()
     // Window dimensions
     int const width{800}, height{400};
     InitWindow(width, height, "Dapper Dasher");
-
+    int velocity{0}, posX{width/2}, posY{height/2};
+    int const r_width{50}, r_height{50};
+    Color const r_color{BLUE};
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        if (IsKeyPressed(KEY_SPACE)) {velocity = -10;} else {velocity = 0;}
+        posY += velocity;
         BeginDrawing();
         ClearBackground(WHITE);
+        DrawRectangle(posX, posY, r_width, r_height, r_color);
         EndDrawing();
     }
     CloseWindow();
