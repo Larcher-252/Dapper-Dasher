f504701aacc6f060725353ced76275254ff686a9
Void7 <limen191100@gmail.com>
2021-10-25 23:37:50 +0300

    Gravity and Ground check
    Add check is player on the ground, and if not then gravity moves him down.

--- dasher.cpp
+++ dasher.cpp
@@ -3,16 +3,31 @@
 int main()
 {
     // Window dimensions
-    int const width{800}, height{400};
+    const int width{800}, height{400};
     InitWindow(width, height, "Dapper Dasher");
-    int velocity{0}, posX{width/2}, posY{height/2};
-    int const r_width{50}, r_height{50};
-    Color const r_color{BLUE};
+    // World params
+    const int gravity{1};
+    int velocity{0};
+    // Rectangle params
+    const int r_width{50}, r_height{50};
+    const Color r_color{BLUE};
+    int posX{width/2}, posY{height/2};
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-        if (IsKeyPressed(KEY_SPACE)) {velocity = -10;} else {velocity = 0;}
+        // Gravity and Ground check
+        if (posY + r_height < height) {velocity += gravity;} 
+        else 
+        {
+            velocity = 0;
+            posY = height - r_height;
+        }
+        // Jump 
+        if (IsKeyPressed(KEY_SPACE)) {velocity -=10;}
+        // Location
         posY += velocity;
+
         BeginDrawing();
         ClearBackground(WHITE);
         DrawRectangle(posX, posY, r_width, r_height, r_color);
